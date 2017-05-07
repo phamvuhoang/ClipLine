f972d860761a7917c566fefde480d14ccf10c4c4
Pham Vu Hoang <Pham Vu Hoang>
2017-05-07 17:06:23 +0700

    compare: update status, call send report

--- app/src/main/java/jp/clipline/clandroid/CompareActivity.java
+++ app/src/main/java/jp/clipline/clandroid/CompareActivity.java
@@ -3,23 +3,29 @@ package jp.clipline.clandroid;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Map;
 
 import jp.clipline.clandroid.Utility.AndroidUtility;
+import jp.clipline.clandroid.api.MediaKey;
+import jp.clipline.clandroid.api.Report;
 
 
 public class CompareActivity extends AppCompatActivity implements View.OnClickListener {
@@ -44,6 +50,15 @@ public class CompareActivity extends AppCompatActivity implements View.OnClickLi
     private Map<String, Object> mCurrentTodoContent;
     private boolean mIsCheckSwitch = true;
 
+    private LinearLayout mLinearLayoutFooterStatus;
+    private ImageView mImageViewFooterView;
+    private TextView mTextViewFooterView;
+    private ImageView mImageViewFooterShoot;
+    private TextView mTextViewFooterShoot;
+    private ImageView mImageViewFooterCompare;
+    private TextView mTextViewFooterCompare;
+
+    private Button mButtonSummit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -184,7 +199,39 @@ public class CompareActivity extends AppCompatActivity implements View.OnClickLi
 //        videoView = (VideoView) findViewById(R.id.videoViewMine);
 //        videoView.setVideoURI(mTodoContentData);
 //        videoView.start();
-    }
+
+
+        // Statuses on Footer
+        mLinearLayoutFooterStatus = (LinearLayout) findViewById(R.id.linearLayoutFooterStatus);
+        mImageViewFooterView = (ImageView) findViewById(R.id.imageViewFooterView);
+        mTextViewFooterView = (TextView) findViewById(R.id.textViewFooterView);
+        mImageViewFooterShoot = (ImageView) findViewById(R.id.imageViewFooterShoot);
+        mTextViewFooterShoot = (TextView) findViewById(R.id.textViewFooterShoot);
+        mImageViewFooterCompare = (ImageView) findViewById(R.id.imageViewFooterCompare);
+        mTextViewFooterCompare = (TextView) findViewById(R.id.textViewFooterCompare);
+
+        // Firstly, hide the status, after getting result from api, then depend on the flags to process the view/hide
+        mLinearLayoutFooterStatus.setVisibility(View.GONE);
+        mImageViewFooterView.setVisibility(View.GONE);
+        mTextViewFooterView.setVisibility(View.GONE);
+        mImageViewFooterShoot.setVisibility(View.GONE);
+        mTextViewFooterShoot.setVisibility(View.GONE);
+        mImageViewFooterCompare.setVisibility(View.GONE);
+        mTextViewFooterCompare.setVisibility(View.GONE);
+
+        updateStatus();
+
+
+        // この内容で提出
+        mButtonSummit = (Button) findViewById(R.id.buttonSubmit);
+        mButtonSummit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // First, get media key
+                // On post execute, upload file to S3 and call report submit api
+                new GetMediaKeyTask().execute(AndroidUtility.getCookie(getApplicationContext()));
+            }
+        });    }
 
     ///// 20170507 ADD START
     @Override
@@ -255,4 +302,145 @@ public class CompareActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
     ///// 20170507 ADD END
+
+    /**
+     * Update footer icons based on return from server
+     */
+    private void updateStatus() {
+        Map<String, Object> todoContent = ((ClWebWrapperApplication) getApplication()).getCurrentTodoContent();
+
+        if (todoContent != null) {
+            boolean hasPlayAction = false;
+            if (todoContent.get("has_play_action") != null) {
+                hasPlayAction = (boolean) todoContent.get("has_play_action");
+
+                // Only when has_play_action is true, then all status will be visible
+                if (hasPlayAction) {
+                    mLinearLayoutFooterStatus.setVisibility(View.VISIBLE);
+                    mImageViewFooterView.setVisibility(View.VISIBLE);
+                    mTextViewFooterView.setVisibility(View.VISIBLE);
+                    // TODO 点灯
+                    if ((todoContent.get("is_play_action_cleared") != null)
+                            && ((boolean) todoContent.get("is_play_action_cleared"))) {
+
+                    }
+                }
+            }
+
+            // Check has_report_action
+            if ((todoContent.get("has_report_action") != null)
+                    && ((boolean)todoContent.get("has_report_action"))) {
+
+                // 表示
+                mLinearLayoutFooterStatus.setVisibility(View.VISIBLE);
+                mImageViewFooterShoot.setVisibility(View.VISIBLE);
+                mTextViewFooterShoot.setVisibility(View.VISIBLE);
+            }
+
+            // check has_my_report_play_action
+            if ((todoContent.get("has_my_report_play_action") != null)
+                    && ((boolean)todoContent.get("has_my_report_play_action"))) {
+
+                // 表示
+                mLinearLayoutFooterStatus.setVisibility(View.VISIBLE);
+                mImageViewFooterCompare.setVisibility(View.VISIBLE);
+                mTextViewFooterCompare.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+
+    /**
+     * Get media key to send file to S3 and send report
+     */
+    public class GetMediaKeyTask extends AsyncTask<String, Void, Boolean> {
+
+        Map<String, Object> mediaKey = null;
+
+        GetMediaKeyTask() {
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            try {
+                String cookie = params[0];
+
+                mediaKey = MediaKey.getMediaKeyContent(cookie);
+                return Boolean.TRUE;
+            } catch (IOException e) {
+                return Boolean.FALSE;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            if (success) {
+                // TODO send file to S3???
+
+                if ((mediaKey != null)
+                        && (mediaKey.get("object_key") != null)) {
+                    Map<String, String> todoParameters = ((ClWebWrapperApplication) getApplication()).getTodoParameters();
+                    String todoContentId = todoParameters.get("todoContentId");
+
+                    String objectKey = (String)mediaKey.get("object_key");
+
+                    // TODO change to right value
+                    String contentType = "video/mp4";
+                    String mediaURLInDevice = "//content:/....";
+                    String mediaDuration = "10:00";
+                    String takenAt = "2016-05-07 10:11:12";
+
+                    // Call report API
+                    new SendReportTask().execute(AndroidUtility.getCookie(getApplicationContext()),
+                            objectKey, contentType, mediaURLInDevice, mediaDuration, takenAt, todoContentId);
+
+                }
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+        }
+    }
+
+
+    public class SendReportTask extends AsyncTask<String, Void, Boolean> {
+
+        Map<String, Object> reponseData = null;
+
+        SendReportTask() {
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            try {
+                String cookie = params[0];
+                String mediaKey = params[1];
+                String contentType = params[2];
+                String mediaURLInDevice = params[3];
+                String mediaDuration = params[4];
+                String takenAt = params[5];
+                String todoContentId = params[6];
+
+                reponseData = Report.sendStudentReport(cookie, mediaKey, contentType, mediaURLInDevice, mediaDuration, takenAt, todoContentId);
+
+                return Boolean.TRUE;
+            } catch (IOException e) {
+                return Boolean.FALSE;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            if (success) {
+                Log.i("reponse:", reponseData.toString());
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+        }
+    }
+
+
 }
